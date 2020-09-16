7c2a14f1182fd0378b899c19efd8129d0b1512a7
Justin Dury-Agri <justin.dury.agri@gmail.com>
2020-09-16 10:38:45 -0700

    (updated) small touch-up to action.js on addURL action definition. added pseudocode as well

--- client/actions/action.js
+++ client/actions/action.js
@@ -1,24 +1,23 @@
 import * as types from '../constants/actionTypes';
 import axios from 'axios';
 
-export function addURL (username, url) {
-  return function dispatchFetchToStore (dispatch) {
-    axios.post('http://localhost:3333/main/addURL', {url})
-      .then((result)=>{
-        dispatch({
-          type: types.FINISHED_URL_ADD,
-          payload: {
-            username,
-            url_id: result.data.url_id,
-            status: result.data.status,
-            url,
-          }
-        });
-      })
-      .catch(err=>
-        console.log('err onsubform', err)
-      )
-  }
+//this is using thunk when it gets to store. Thunk receives the first dispatch, which is a function with the argument 'dispatch' passed in. It recognizes this and performs the async action within, which, in this case, is a fetch request to the url passed in.
+export const addURL (username, url) => (dispatch) => {
+  axios.post('http://localhost:3333/main/addURL', {url})
+    .then((result)=>{
+      dispatch({
+        type: types.FINISHED_URL_ADD,
+        payload: {
+          username,
+          url_id: result.data.url_id,
+          status: result.data.status,
+          url,
+        }
+      });
+    })
+    .catch(err=>
+      console.log('err onsubform', err)
+    )
 };
 
 export const checkNow = (statusObj) => (

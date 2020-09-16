a3a13594897d0f8491f7c3938a6df252546bbe51
b-hash <brianmhayashi@gmail.com>
2020-09-16 09:07:13 -0700

    not sure

--- client/actions/action.js
+++ client/actions/action.js
@@ -1,11 +1,11 @@
-import * as types from '../constants/actionTypes';
 import axios from 'axios';
+import * as types from '../constants/actionTypes';
 
-export function addURL (username, url) {
-  return function dispatchFetchToStore (dispatch) {
+export function addURL(username, url) {
+  return function dispatchFetchToStore(dispatch) {
     axios.post('http://localhost:3333/main/addURL', `${url}`)
-      .then((result)=>{
-        console.log("YOO!!")
+      .then((result) => {
+        console.log('YOO!!');
         dispatch({
           type: types.FINISHED_URL_ADD,
           payload: {
@@ -13,17 +13,15 @@ export function addURL (username, url) {
             url_id: result.data.url_id,
             status: result.data.status,
             url,
-          }
+          },
         });
       })
-      .catch(err=>
-        console.log('err onsubform', err)
-      )
-  }
-};
+      .catch((err) => console.log('err onsubform', err));
+  };
+}
 
 export const checkNow = (statusObj) => (
-  console.log("we here"),
+  console.log('we here'),
   {
     type: types.CHECK_NOW,
     payload: statusObj,
@@ -31,6 +29,5 @@ export const checkNow = (statusObj) => (
 );
 
 export const finishedUrlAdd = (addedUrlObj) => ({
-  
-  
+
 });
--- client/components/InputBox.jsx
+++ client/components/InputBox.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
 // import { connect } from 'react-redux';
-// import { addURL } from '../actions/action.js'; 
+// import { addURL } from '../actions/action.js';
 // import axios from 'axios';
 // import * as actions from "../actions/action.js"
 
-
 class InputBox extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.onSubForm = this.onSubForm.bind(this);
   }
 
-  onSubForm (e) {
+  onSubForm(e) {
     e.preventDefault();
-    const input = document.getElementById("addUrlForm");
+    const input = document.getElementById('addUrlForm');
     const url = input.value;
     const username = this.props.currentUser;
     // sends URL to store/reducer, which then THUNKS, sends a fetch to back end,
     // and dispatches the result to store
     this.props.addURL(username, url);
-    
+
     // axios.post('http://localhost:3000/main/addURL', `${url}`)
     // // gets back something
     // .then((result)=>{
@@ -38,15 +37,12 @@ class InputBox extends React.Component {
   render() {
     return (
       <div id="inputBox">
-          <input id="addUrlForm" type="text" name="url"></input>
-          <button type="submit" onClick={this.onSubForm}>Add url</button>
+        <input id="addUrlForm" type="text" name="url" />
+        <button type="submit" onClick={this.onSubForm}>Add url</button>
       </div>
 
-    )
-  };
+    );
+  }
 }
 
 export default InputBox;
-
-
-
